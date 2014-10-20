8e2ee84ff89db80f4f2ec48e2d04910b45690ed5
尹晓腾 <YinXiaoteng@yinxiaotengdeMacBook-Pro.local>
2014-10-20 17:10:52 +0800

    Initial Commit

---
+++ 作业12/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  作业12
+//
+//  Created by 尹晓腾 on 14-10-20.
+//  Copyright (c) 2014年 bawei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
